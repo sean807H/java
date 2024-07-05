2f06228226134e430523473c5f3df2c847ac8bb9
김서연 <129020628+sean807H@users.noreply.github.com>
2024-07-05 10:35:01 +0900

    2학년 1학기

---
+++ 2-1/ArrayListEx.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> a = new ArrayList<String>();
+		
+		Scanner scanner = new Scanner(System.in);
+		for(int i = 0; i < 4; i++) {
+			System.out.print("이름을 입력하세요 >> ");
+			String s = scanner.next();
+			a.add(s);
+		}
+		// ArrayList에 들어잇는 모든 이름 출력
+		for(int i=0; i<a.size(); i++) {
+			String name = a.get(i);
+			System.out.println(name+" ");
+		}
+		// 가장 긴 이름 출력
+		int longestIndex = 0;
+		for(int i=1; i<a.size(); i++) {
+			if(a.get(longestIndex).length() < a.get(i).length())
+				longestIndex = i;
+		}
+		System.out.println("\n가장 긴 이름은 : "+a.get(longestIndex));
+		
+		// 써줘야 안전함
+		scanner.close();
+	}
+	
+}
