c43c68e01389d5acdc47654f1579654c6b73d097
김서연 <129020628+sean807H@users.noreply.github.com>
2024-07-31 23:34:26 +0900

    2학년 2학기

---
+++ 2-2/FileWriterEx.java
@@ -0,0 +1,30 @@
+package file;
+
+import java.io.*;
+import java.util.*;
+
+public class FileWriterEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		FileWriter fout = null;
+		int c;
+		try {
+			fout = new FileWriter(".\\src\\file\\filetext.txt");
+			while(true) {
+				String line = scanner.nextLine();
+				if(line.length()==0) {
+					break;
+				}
+				fout.write(line,0,line.length());
+				fout.write("\r\n",0,2);
+			}
+			fout.close();
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+		scanner.close();
+	}
+
+}
